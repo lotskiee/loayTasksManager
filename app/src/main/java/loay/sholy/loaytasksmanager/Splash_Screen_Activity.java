a9cb07a56971b8ffa7b0dfa959ef94d035c9ee29
Owner <lama76618@gmail.com>
2021-11-09 13:21:22 +0200

    1.after designing all activities 2.all activities graphics components (view) are decleared and foucd view by id

--- app/src/main/java/loay/sholy/loaytasksmanager/Splash_Screen_Activity.java
+++ app/src/main/java/loay/sholy/loaytasksmanager/Splash_Screen_Activity.java
@@ -2,6 +2,7 @@ package loay.sholy.loaytasksmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -13,16 +14,22 @@ public class Splash_Screen_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         imgSplash=findViewById(R.id.imgSplash);
+        // thread#1
         Thread th=new Thread(){
+            //thread#2
             @Override
             public void run() {
-                int i=3*1000;
+                //thread#3
+                int ms =3*1000;
                 try {
-                    sleep(i);
+                    sleep(ms);
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));// transfers from splash screen to main activity page.
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
+        //thread#4
+        th.start();
     }
 }
